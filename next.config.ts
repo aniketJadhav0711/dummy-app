0a3dab0ac0976012b4b025df8813bd423ca95857
aniketJadhav0711 <aniket_jadhav@amicusglobal.com>
2025-08-22 13:50:04 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/dfsmedia/:slug*",
+  //       destination: "https://sitecore.test.jlgagriculture.com/dfsmedia/:slug*",
+  //     },
+  //   ];
+  // },
+  images: {
+    dangerouslyAllowSVG: true,
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "sitecore.test.jlgagriculture.com",
+    //     port: "",
+    //     pathname: "/**",
+    //   },
+    // ],
+  },
 };
 
 export default nextConfig;
