df8976bd4a45800344ef8da7c6665cd3dde1f027
aniketJadhav0711 <aniket_jadhav@amicusglobal.com>
2025-08-22 14:13:47 +0530

    Remove unused Image component and log environment variable in Home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,19 +12,13 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_TEST);
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          src="/dfsmedia/e4042b10c9ce4595b4cc059f1299f079/164541-source/925ag-imperial-01"
-          alt="Description of image"
-          className="object-fill aspect-square h-full w-full"
-          width={342}
-          height={337}
-          priority
-        />
+        hello world
       </main>
     </div>
   );
